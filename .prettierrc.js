f5cbe857f1bcadf5cc1f26a1224b67328cac7d90
NewCicada <1547598968@qq.com>
2023-01-17 14:09:14 +0800

    feat:增加依赖

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports =  {
+  "printWidth": 120,
+  "semi": false,
+  "singleQuote": true,
+  "trailingComma": "all",
+  "bracketSpacing": false,
+  "jsxBracketSameLine": true,
+  "arrowParens": "avoid",
+  "insertPragma": true,
+  "tabWidth": 4,
+  "useTabs": false  
+};
