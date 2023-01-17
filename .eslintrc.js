3f80defba1e300d2adb7c0ce8d4b57a85b99becf
NewCicada <1547598968@qq.com>
2023-01-17 14:57:33 +0800

    feat: 增加依赖

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,24 @@
 module.exports = {
-
-  parser:  '@typescript-eslint/parser',
-  extends: [
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended'
-  ],                              //使用推荐的React代码检测规范
-  plugins: ['@typescript-eslint'],
-  env:{                         
-      browser: true,
-      node: true,
-  },
-  settings: {             //自动发现React的版本，从而进行规范react代码
-      "react": {
-          "pragma": "React",
-          "version": "detect"
-      }
-  }, 
-  parserOptions: {        //指定ESLint可以解析JSX语法
-      "ecmaVersion": 2019,
-      "sourceType": 'module',
-      "ecmaFeatures":{
-          jsx:true
-      }
-  },
-  rules: {
-  
-  }
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
 }
